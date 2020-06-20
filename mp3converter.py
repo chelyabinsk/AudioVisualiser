17a0f1009973ae64f9b76e8399bac5f77840aad0
Pietro Petitti <19675218+BambooFlower@users.noreply.github.com>
2020-06-20 12:13:56 +0100

    Delete mp3converter.py

--- mp3converter.py
+++
@@ -1,8 +0,0 @@
-# Convert the audio file from mp3 to wav
-# A wav is need for the visualization 
-# 
-# Change EXAMPLE with the desired mp3 file 
-
-from pydub import AudioSegment
-sound = AudioSegment.from_mp3("EXAMPLE.mp3")
-sound.export("EXAMPLE.wav", format="wav")
